f74081675df8c8aaf5cca80305e5f1d2f7d0c4fe
Robert Herget <RHerget_H@dev.bse.edu>
2015-06-25 09:24:14 -0500

    Added pagination start

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -3,11 +3,11 @@ $(document).ready(getposts);
 function getblog(){
 	$.getJSON("entries.json", function(data){
 		$.each(data.entries, function(key, val){
-			//if (key < 5){// now 5 posts show up at a time 
-			console.log(key);
-			$("#blog").prepend("<p>" + val.text + "</p>");
-			$("#blog").prepend("<h6>" + val.date + "</h6>");
-			$("#blog").prepend("<h5>" + val.title + "</h5>");	
+			if (key < 5){// now 5 posts show up at a time 
+				console.log(key);
+				$("#blog").prepend("<p>" + val.text + "</p>");
+				$("#blog").prepend("<h6>" + val.date + "</h6>");
+				$("#blog").prepend("<h5>" + val.title + "</h5>");	
 			}	
 		});
 	});
