551134decd5d387c3369b21ec4ab6aa6feeadce3
Robert Herget <RHerget_H@dev.bse.edu>
2015-06-25 16:09:21 -0500

    Testing key.length

--- assets/js/blogger.js
+++ assets/js/blogger.js
@@ -4,8 +4,9 @@ function getblog(){
 	pager();
 	$.getJSON("entries.json", function(data){
 		$.each(data.entries, function(key, val){
-			if (key < 5){// now 5 posts show up at a time 
+			if (key.length < 5){// now 5 posts show up at a time 
 				console.log(key);
+				console.log(key.length);
 				$("#blog").prepend("<p>" + val.text + "</p>");
 				$("#blog").prepend("<h6>" + val.date + "</h6>");
 				$("#blog").prepend("<h5>" + val.title + "</h5>");	
@@ -15,11 +16,17 @@ function getblog(){
 }
 
 function pager(){
-	$("<input type='button' name='prev' value='Prev'/>").click(function() {
+	$("<input type='button' name='prev' value='Prev'/>").click(function(){
+		//function code goes here!
+		//set interval decrement key length
+		//hide on page one
 		alert('hi2');
 		//here i need functionality to get the last 5 posts and display them or hide the button if first page
 	}).appendTo("#blog");
-	$("<input type='button' name='next' value='Next'/>").click(function() {
+	$("<input type='button' name='next' value='Next'/>").click(function(){
+		//function code goes here~!
+		//set interval increment key lenght 
+		//hide on page last 
 		alert('hi');
 		//here I need to get the next 5 posts and display them or hide the button if last page
 	}).appendTo("#blog");
@@ -28,3 +35,18 @@ function pager(){
 //check what page im on or do it check which posts are displaying
 //then load up the page based on next or prev. 
 //
+'''
+
+var interval = undefined;//use for a fadein et unnescessaris
+
+function get_prev(){
+	var $curr = $("#blog"),
+	$next = ($curr.prev().length) ? $curr.prev() : $("#blog").last()
+};
+
+function get_next(){
+	var $curr = $("#blog"), 
+	$next = ($curr.next().length) ? $curr.next() : $("#blog").first();
+};
+'''
+
